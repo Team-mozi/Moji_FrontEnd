4a6562211c18f2cbaf1b062d1afa2b040bfcd3a6
ParkJunYoung <j77777y@naver.com>
2025-10-03 01:26:26 +0900

    [Feat] #26 : root ToastProvider 연결

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import './index.css'
-import { Provider } from 'react-redux'
-import { RouterProvider } from 'react-router-dom'
+import './index.css';
+import { Provider } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
 
-import router from '@/routes'
-import { store } from '@/store/store'
+import ToastProvider from '@/components/ToastProvider';
+import router from '@/routes';
+import { store } from '@/store/store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <ToastProvider>
+        <RouterProvider router={router} />
+      </ToastProvider>
     </Provider>
   </StrictMode>,
-)
+);
